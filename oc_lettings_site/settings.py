c391967569b927736f6d4fd89a4ffda3012748ca
Biodja <danielsenavon@gmail.com>
2023-03-31 01:03:06 +0200

    settings

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://p13-open-class-room.herokuapp.com/' , '127.0.0.1']
 
 
 # Application definition
@@ -108,7 +108,7 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = True 
 
 USE_TZ = True
 
