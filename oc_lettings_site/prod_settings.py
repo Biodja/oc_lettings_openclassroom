76d34449ac31fad15ddc78e38c20fcfa388d9c6c
Biodja <danielsenavon@gmail.com>
2023-03-31 14:40:34 +0200

    database12

--- oc_lettings_site/prod_settings.py
+++ oc_lettings_site/prod_settings.py
@@ -1,7 +1,9 @@
 from .settings import *
 import dj_database_url
 
-#DATABASES['default'] = dj_database_url.config()
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 
 
 DEBUG = False
